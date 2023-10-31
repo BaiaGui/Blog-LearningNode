e2bebeb48593709875b2a51b745a96d20616df73
Guilherme Baia <guilhermebaiav@gmail.com>
2023-10-31 03:06:05 -0300

    creating endpoints

--- server.js
+++ server.js
@@ -6,12 +6,62 @@
 // });
 
 // server.listen(3000);
-import express from 'express';
+import express, { response } from 'express';
+import { BlogDatabase } from './database.js';
 const app = express();
+app.use(express.json());
+
 const port = 3000;
+
+const database = new BlogDatabase();
 
 app.get('/', (req, res)=>{
     res.send('Hello World');
+})
+
+app.get('/blog', (req, res)=>{
+    const searchParam = req.query.search
+    const blogs = database.listBlogs(searchParam);
+    return res.send(blogs);
+})
+
+app.post('/blog', (req, res)=>{
+    const {title, description, blogBody} = req.body
+    database.createBlog({
+        title,
+        description,
+        blogBody,
+    })
+    return res.status(201).end();
+})
+
+
+
+
+app.patch('/blog/:id', (req, res)=>{
+
+})
+
+app.put('/blog/:id', (req, res)=>{
+
+    const blogId = req.params.id;
+
+    const {title, description, blogBody} = req.body;
+
+    database.updateBlog(blogId, {
+        title,
+        description,
+        blogBody,
+    })
+    return res.status(204).send();
+})
+
+
+app.delete('/blog/:id', (req, res)=>{
+    const blogId = req.params.id;
+    database.deleteBlog(blogId);
+    return res.status(204).send();
+
 })
 
 
